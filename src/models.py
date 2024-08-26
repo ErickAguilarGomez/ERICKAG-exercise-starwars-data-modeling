b680bd1fd7431d30238a5baacbe17f1591eca6fc
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-26 23:00:02 +0000

    Terminando bd

--- src/models.py
+++ src/models.py
@@ -7,23 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favoritos = relationship("Favoritos",backref="owner",lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name= Column(String(250), nullable=False)
+    height= Column(String(250), nullable=False)
+    gender= Column(String(250), nullable=False)
+    favoritos = relationship("Favoritos",backref="owner",lazy=True)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(250), nullable=False)
+    climate= Column(String(10), nullable=False)
+    terrain= Column(String(10), nullable=False)
+    favoritos = relationship("Favoritos",backref="owner",lazy=True)
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name= Column(String(250), nullable=False)
+    model= Column(String(10), nullable=False)
+    class_name=Column(String(10), nullable=False)
+    favoritos = relationship("Favoritos",backref="owner",lazy=True)
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+
+    usuario_id = Column(Integer, ForeignKey('usuario.id'),nullable=False)
+    character_id= Column(Integer, ForeignKey('character.id'),nullable=True)
+    planet_id= Column(Integer, ForeignKey('planet.id'),nullable=True)
+    vehicle_id= Column(Integer, ForeignKey('vehicle.id'),nullable=True)
+
+
+
 
     def to_dict(self):
         return {}
